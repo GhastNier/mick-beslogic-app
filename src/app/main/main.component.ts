4303cadb3dc57b939a2d255826410a187b6c51a1
Michael Savard <89518368+GhastNier@users.noreply.github.com>
2023-02-16 20:00:18 -0500

    Hadding Favorites

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,22 +10,17 @@ import {HttpClient} from "@angular/common/http";
 })
 export class MainComponent implements OnInit {
   pokemonList: any[] = [];
-
+  loading: boolean = false;
   ngOnInit() {
     const randomIds = this.generateRandomIds(905, 120);
     const requests = randomIds.map(id =>
       this.http.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
     );
     forkJoin(requests).subscribe(pokemonList => {
-      this.pokemonList = pokemonList;
-      console.log(this.pokemonList.at(0).name);
-      console.log(this.pokemonList.at(1).name);
-      console.log(this.pokemonList.at(2).name);
-      console.log(this.pokemonList.at(3).name);
-      console.log(this.pokemonList.at(4).name);
-      console.log(this.pokemonList.at(5).name);
-      console.log(this.pokemonList.at(6).name);
-      console.log(this.pokemonList.at(7).name);
+      this.pokemonList = pokemonList.map(pokemon => ({
+        ...pokemon,
+        favorite: false // add a 'favorite' property to each pokemon object
+      }));
     });
   }
 
@@ -38,13 +33,11 @@ export class MainComponent implements OnInit {
         randomIds.push(id);
       }
     }
-    randomIds.sort((a,b)=>a-b)
+    randomIds.sort((a, b) => a - b)
     console.log(randomIds.toString())
     return randomIds;
   }
-
-  loading: boolean = false;
-
   constructor(private breakpointObserver: BreakpointObserver, private http: HttpClient) {
   }
+
 }
